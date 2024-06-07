fd3a3c9d7cf80728c2edf75e657513129bf53303
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-06-07 13:04:02 +0700

    feat(App.jsx): create initFetch(); rendering fetch data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const initFetch = async () => {
+    const response = await axios.get("http://localhost:4001/products");
+    const products = response.data.data;
+    // console.log(products);
+    // console.log(products[0].description);
+    setSearchResult(products);
+    console.log(searchResult);
+  };
+  useEffect(() => {
+    initFetch();
+  }, []);
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
+        {searchResult.map((product, index) => {
+          return (
+            <div className="product" key={product.id}>
+              <div className="product-preview">
+                <img
+                  src={product.image}
+                  alt="some product"
+                  width="350"
+                  height="350"
+                />
+              </div>
+              <div className="product-detail">
+                <h1>Product name: {product.name}</h1>
+                <h2>Product price: {product.price} Baht</h2>
+                <p>Product description: {product.description}</p>
+              </div>
 
-          <button className="delete-button">x</button>
-        </div>
+              <button className="delete-button">x</button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
